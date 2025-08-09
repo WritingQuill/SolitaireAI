b0bc72d928b30bb02fcc3109f7e0a9063cd29ea0
Kev <llanajkevin@gmail.com>
2025-08-09 20:10:14 +0200

    Added the click on draw pile method

--- SOLITAIRE_CLASS.py
+++ SOLITAIRE_CLASS.py
@@ -5,11 +5,11 @@ class SOLITAIRE:
         #self.name = name
         self.already_assigned_cards = []
         self.playing_field = self.generate_playingfield()
-        self.hidden_pile = [] #Ziehstapel
+        self.hidden_pile = self.generate_hidden_pile() #Ziehstapel
         self.draw_pile_with_assigned_cards = [] #Ablagestapel
+        self.drawn_card= 0
         self.foundation = {} #Endstapel
-        print(self.playing_field)
-        print(self.already_assigned_cards)
+   
     
     def generate_playingfield(self):
         playing_field = np.zeros((13, 7), dtype=object)
@@ -27,14 +27,27 @@ class SOLITAIRE:
 
         return playing_field   
     
-    def click_on_draw_pile(self):
         
     def generate_hidden_pile(self):
+        hidden_pile = []
         for i in range(24):   
             random_number = random.randint(1,52)
             while random_number in self.already_assigned_cards:
                 random_number = random.randint(1, 52)
             self.already_assigned_cards.append(random_number)   
-            self.hidden_pile.append(random_number)
-        return self.hidden_pile
+            hidden_pile.append(random_number)
+        return hidden_pile
+    
+    def click_on_draw_pile(self):
+        if self.drawn_card == 0:
+            drawn_card = self.hidden_pile[0]
+        else: 
+            index_hidden_pile = self.hidden_pile.index(self.drawn_card)
+            try:
+                drawn_card = self.hidden_pile[index_hidden_pile + 1]
+            except IndexError:
+                drawn_card = self.hidden_pile[0]
+        self.drawn_card = drawn_card
+        return 
+
 Test = SOLITAIRE()
